73cfcc636cec36c4131452bef48f9d1f48948835
rnd <rnd@rnd.com>
2015-05-06 21:58:44 +0200

    ..

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 dofile(minetest.get_modpath("basic_machines").."/mark.lua") -- used for markings, borrowed and adapted from worldedit
 dofile(minetest.get_modpath("basic_machines").."/mover.lua")
 
+-- IF USING UNTERNULL GAME COMMENT THE FOLLOWING
 minetest.after(1, function() -- if you want keypad to open doors 
 	dofile(minetest.get_modpath("basic_machines").."/mesecon_doors.lua")
 end)
