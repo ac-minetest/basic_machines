d6c6b3e6daa86f3987c7126053381ded334f41d4
rnd <rnd@rnd.com>
2015-05-06 16:27:52 +0200

    ..

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 dofile(minetest.get_modpath("basic_machines").."/mark.lua") -- used for markings, borrowed and adapted from worldedit
 dofile(minetest.get_modpath("basic_machines").."/mover.lua")
 
-minetest.after(1, function() -- if you want keypad to open doors
+minetest.after(1, function() -- if you want keypad to open doors 
 	dofile(minetest.get_modpath("basic_machines").."/mesecon_doors.lua")
 end)
