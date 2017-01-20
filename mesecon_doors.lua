55fecb3a1aa1649f9e16d1be771b19d6866750c8
Robbie Ferguson <contact@category5.tv>
2017-01-20 13:31:26 -0500

    Fix crash on pressure plate

--- mesecon_doors.lua
+++ mesecon_doors.lua
@@ -12,6 +12,7 @@ local function door_signal_overwrite(name)
 	-- this will make door toggle whenever its used
 	table2.mesecons = {effector = {
 		action_on  =  function (pos,node,ttl)
+			if type(ttl) ~= "number" then ttl = 1 end
 			if ttl<0 then return end
 			local meta = minetest.get_meta(pos);local name = meta:get_string("doors_owner");
 			-- create virtual player
