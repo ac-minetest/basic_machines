5c93f753e6840409df9aeb501be418e891f37568
berengma <Gundul@mailbox.org>
2018-07-29 14:39:54 +0200

    updated array of farming_redo plants for mover

--- mover.lua
+++ mover.lua
@@ -67,12 +67,15 @@ basic_machines.plant_table  = {["farming:seed_barley"]="farming:barley_1",["farm
 ["farming:seed_cotton"]="farming:cotton_1",["farming:cucumber"]="farming:cucumber_1",["farming:grapes"]="farming:grapes_1",
 ["farming:melon_slice"]="farming:melon_1",["farming:potato"]="farming:potato_1",["farming:pumpkin_slice"]="farming:pumpkin_1",
 ["farming:raspberries"]="farming:raspberry_1",["farming:rhubarb"]="farming:rhubarb_1",["farming:tomato"]="farming:tomato_1",
-["farming:seed_wheat"]="farming:wheat_1"}
+["farming:seed_wheat"]="farming:wheat_1",["farming:seed_hemp"]="farming:hemp_1",["farming:chili_pepper"]="farming:chili_1",
+["farming:garlic_clove"]="farming:garlic_1",["farming:onion"]="farming:onion_1",["farming:peppercorn"]="farming:pepper_1",
+["farming:pineapple_top"]="farming:pineapple_1",["farming:potato"]="farming:potato_1",["farming:pea_pod"]="farming:pea_1",
+["farming:beetroot"]="farming:beetroot_1"}
 
 -- list of objects that cant be teleported with mover
 basic_machines.no_teleport_table = {
 ["itemframes:item"] = true,
-["signs:text"] = true
+["signs:text"] = true,
 }
 
 -- list of nodes mover cant take from in inventory mode
